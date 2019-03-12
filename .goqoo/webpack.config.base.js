8dd0a7ec810453bad21882a094fe1a9298a2c187
t-kojima <t-kojima@sonicgarden.jp>
2019-03-12 16:40:38 +0900

    add: project

---
+++ .goqoo/webpack.config.base.js
@@ -0,0 +1,133 @@
+/* eslint-disable no-console */
+const webpack = require('webpack')
+const path = require('path')
+const rcfile = require('rc-config-loader')
+require('dotenv').config()
+const DropboxKintone = require('./dropbox')
+const S3Plugin = require('webpack-s3-plugin')
+
+const { npm_package_name: projectName } = process.env
+const { apps, useDropbox } = rcfile('goqoo', { configFileName: path.resolve('config', 'goqoo.config') }).config
+
+const entry = apps.reduce((obj, appName) => {
+  obj[appName] = ['babel-polyfill', path.resolve('apps', appName)]
+  return obj
+}, {})
+
+const output = { path: path.resolve('dist') }
+if (useDropbox) {
+  const dropbox = {
+    rootDir: process.env.DROPBOX_ROOT,
+    subDir: process.env.DROPBOX_KINTONE_DIR,
+    token: process.env.DROPBOX_TOKEN,
+  }
+
+  if (!dropbox || !dropbox.rootDir) {
+    console.error('DROPBOX_ROOT: environment variable not found!')
+    process.exit(1)
+  }
+
+  const outputDir = `${dropbox.subDir || ''}/${projectName}`
+  output.path = path.resolve(path.join(dropbox.rootDir, outputDir))
+
+  if (dropbox.token) {
+    const dbxKintone = new DropboxKintone({
+      accessToken: dropbox.token,
+      localRootDir: dropbox.rootDir,
+    })
+    const dbxOutputPaths = apps.map(appName => `/${outputDir}/${appName}.js`)
+    dbxKintone.fetchSharedLinks(dbxOutputPaths).then(paths => {
+      console.log(JSON.stringify(paths, null, '  '))
+    })
+  }
+}
+
+const config = {
+  entry,
+  output,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: { browsers: ['last 2 versions'] },
+                modules: false,
+              },
+            ],
+          ],
+          plugins: [['@babel/proposal-class-properties', { loose: false }]],
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader/useable', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader/useable', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve('dist'),
+    inline: true,
+    // hot: true,
+    https: true,
+    port: 59000,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    progress: true,
+  },
+}
+
+if (process.env.S3) {
+  config.entry = Object.entries(config.entry).reduce((obj, [key, value]) => {
+    obj[`${key}-${process.env.AWS_RANDOM_SUFFIX}`] = value
+    return obj
+  }, {})
+  ;['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_S3_REGION', 'AWS_S3_BUCKET'].forEach(variable => {
+    if (!process.env[variable]) {
+      console.error(`${variable}: environment variable not found!`)
+      process.exit(1)
+    }
+  })
+
+  config.plugins.push(
+    new S3Plugin({
+      // Exclude uploading of html
+      exclude: /.*\.html$/,
+      // s3Options are required
+      s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_S3_REGION,
+      },
+      s3UploadOptions: {
+        Bucket: process.env.AWS_S3_BUCKET,
+      },
+      basePath: process.env.AWS_S3_BASEPATH || projectName,
+    })
+  )
+}
+
+module.exports = config
---
+++ config/webpack.config.goqoo.js
@@ -0,0 +1,20 @@
+const config = require('../.goqoo/webpack.config.base')
+
+// Edit the "config" object as you like.
+// Do not edit ".goqoo/webpack.config.base"
+
+// ex. Add Loaders
+// config.module.rules.push({
+//   test: /\.ext$/,
+//   loader: ['foo-loader', 'bar-loader'],
+// })
+
+// ex. Add Plugins
+// config.plugins.push(new foobarPlugin())
+
+// ex. Change webpack-dev-server Settings
+// config.devServer.hot = true
+
+// And more!
+
+module.exports = config
